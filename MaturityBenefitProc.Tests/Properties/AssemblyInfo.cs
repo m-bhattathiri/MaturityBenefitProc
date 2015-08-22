35ab3c31babb16dc51e4df6fea1a783ccf5bc1bb
Poorva-WW <poorva@wisdomworks.co>
2015-08-22 16:10:00 +0530

    Add test project scaffolding and NuGet config

---
+++ MaturityBenefitProc.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MaturityBenefitProc.Tests")]
+[assembly: AssemblyDescription("Unit tests for MaturityBenefitProc life insurance maturity benefit processing")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Meenakshi Bhattathiri Insurance Services")]
+[assembly: AssemblyProduct("MaturityBenefitProc.Tests")]
+[assembly: AssemblyCopyright("Copyright \u00a9 Meenakshi Bhattathiri Insurance Services 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f7d28a15-9c43-4e6b-8d12-a5b3c9e71f84")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
